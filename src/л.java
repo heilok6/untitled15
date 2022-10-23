f68c2ae9a072b300e4a7de1833c9dd94248cdde0
Администратор <jyhfyjhf@mail.ru>
2022-10-23 14:06:46 +1200

    cycles

---
+++ src/л.java
@@ -0,0 +1,9 @@
+public class л {
+    public static void main(String[] args) {
+        int a = 13;
+        while (a > -33) {
+            System.out.println(a);
+            a = a - 2;
+        }
+    }
+}
